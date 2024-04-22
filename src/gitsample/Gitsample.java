51907116979648b32a4b7c72d8487affe9c9a906
kyoheifujimori <kyohei.fujimori@g.dreamcareer.co.jp>
2024-04-22 12:42:05 +0900

    gitsampleprojectを作成

---
+++ src/gitsample/Gitsample.java
@@ -0,0 +1,11 @@
+package gitsample;
+
+public class Gitsample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは");
+		System.out.println("Hello!");
+	}
+
+}
